c2b6ba92eedac5ea231e01c9187331044b9fd713
Ganghuan Huang <56037905+GH-H@users.noreply.github.com>
2020-10-16 09:42:23 -0700

    instruction decode implemented
    instructions obj: add,subtract,stop,load,store,character output,character input
    
    execute not implemented

---
+++ src/model/instructionType/Sub.java
@@ -0,0 +1,33 @@
+package model.instructionType;
+/**
+ * This class is the subtract instruction
+ */
+
+public class Sub implements Instruction{
+    /** The destination register number. */
+    private String myRd;
+
+    /** The operandSpecifier value. */
+    private String operandSpecifier;
+
+    /** The opcode of the instruction. */
+    private String myOpcode;
+
+
+    public Sub(String myOpcode, String myRd, String operandSpecifier) {
+        this.myRd = myRd;
+        this.operandSpecifier = operandSpecifier;
+        this.myOpcode = myOpcode;
+    }
+
+
+    @Override
+    public String getOpcode() {
+        return myOpcode;
+    }
+
+//    @Override
+//    public void execute() {
+//        //need to be implemented
+//    }
+}
