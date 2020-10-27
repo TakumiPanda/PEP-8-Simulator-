949ecb96c98023899a50d68c3b713331fc9d84a9
garret <garret12@uw.edi>
2020-10-27 14:59:19 -0700

    Update directory structure

---
+++ src/controller/Simulator.java
@@ -0,0 +1,50 @@
+package controller;
+
+import model.ControlUnitImpl;
+import model.MemoryDumpImpl;
+import view.SimulatorWindow;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Accepts input from the View SimulatorWindow and works with the model to update accordingly. 
+ */
+public class Simulator implements Observer {
+
+	private ControlUnitImpl controlUnit;
+	private SimulatorWindow window;
+
+	public Simulator() throws IOException {
+		JFrame frame = new JFrame();
+		frame.setBackground(Color.BLACK);
+		window = new SimulatorWindow((MemoryDumpImpl)new ControlUnitImpl(window).getMemoryDump());
+		controlUnit = new ControlUnitImpl(window);
+		window.addObserver(this);
+		frame.add(window.getMainPanel());
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(3);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		String objectCode = window.getObjectCodeArea().getText();
+		controlUnit.getMemoryDump().updateMemory(objectCode);
+		window.getMemoryArea().setText(controlUnit.getMemoryDump().toString());
+		window.getMemoryArea().setCaretPosition(0);
+		try {
+			window.reset();
+			controlUnit.startCycle();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		//Update the GUI components when fetch-execute cycle is finished.
+		window.setMemoryDump((MemoryDumpImpl)controlUnit.getMemoryDump());
+
+	}
+}
---
+++ tst/utils/TransformerTests.java
@@ -0,0 +1,33 @@
+package utils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TransformerTests {
+    @Test
+    void testHexToDecimal() {
+        String h = "A";
+        int d = Transformer.hexToDecimal(h);
+        Assertions.assertEquals(10,d);
+    }
+
+    @Test
+    void testBinToDecimal() {
+        String b = "1010";
+        int d = Transformer.binToDecimal(b);
+        Assertions.assertEquals(10,d);
+    }
+
+    @Test
+    void testHexToBinary() {
+        String h = "ab";
+        String b = Transformer.hexToBinary(h);
+        Assertions.assertEquals("1010 1011 ",b);
+    }
+
+    @Test
+    void testDecimalToBinary() {
+        int d = 10;
+        String b = Transformer.decimalToBinary(d);
+        Assertions.assertEquals("1010",b);
+    }
+}
