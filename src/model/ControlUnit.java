b6e3a7af11128bdf0c27b74decca37667225483f
garret123h <garret12@uw.edu>
2020-11-06 10:23:49 -0800

    Add methods for ControlUnit Interface

--- src/model/ControlUnit.java
+++ src/model/ControlUnit.java
@@ -1,9 +1,37 @@
 package model;
 
 public interface ControlUnit {
-    
+
     /**
      * Starts the fetch-execute cycle in the Von-Nueman model.
      */
     void startCycle() throws InterruptedException;
+
+    /**
+     * Executes only a single instruction specified
+     */
+
+    void executeSingleInstruction(String instruction);
+
+    /**
+     * Gets the current Operand that is needed when displaying the internal state of
+     * the machine in the View window.
+     */
+    Instruction getCurrentInstructionOperand();
+
+    /**
+     * Controller needs access to the ALU for the internal State of the machine. In
+     * particular it needs to registers state.
+     * 
+     * @return ArithmeticLogicUnit
+     */
+    ArithmeticLogicUnitImpl getALU();
+
+    /**
+     * The Controller needs access to the memory Dump object for both displaying and
+     * manipulating.
+     * 
+     * @return
+     */
+    MemoryDump getMemoryDump();
 }
