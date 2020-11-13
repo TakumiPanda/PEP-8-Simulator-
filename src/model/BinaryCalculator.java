0bedbeaf95b3810715ead8ea3a8cb695b0887746
garret123h <garret12@uw.edu>
2020-11-13 09:32:17 -0800

    Fix structute of code

--- src/model/BinaryCalculator.java
+++ src/model/BinaryCalculator.java
@@ -1,54 +1,57 @@
 package model;
 
-public class BinaryCalculator extends Calculator<Binary> {
+public class BinaryCalculator implements Calculator {
 
 	@Override
-	public Binary add(Binary a, Binary b) {
+	public Binary add(Object num1, Object num2) {
+		Binary a = (Binary) num1;
+		Binary b = (Binary) num2;
 		String aStr = a.getNumber(); //bin str
 		String bStr = b.getNumber(); //bin str
 		int aInt = Integer.parseInt(aStr, 2); //int
 		int bInt = Integer.parseInt(bStr, 2); //int 
 		int result = aInt + bInt; // int
 		String resultStr = Integer.toBinaryString(result); //bin str
-		Binary resultBin = new Binary(resultStr);
-		return resultBin;
+		return new Binary(resultStr);
 	}
 
-
 	@Override
-	public Binary subtract(Binary a, Binary b) {
+	public Binary subtract(Object num1, Object num2){
+		Binary a = (Binary) num1;
+		Binary b = (Binary) num2;
 		String aStr = a.getNumber(); //bin str
 		String bStr = b.getNumber(); //bin str
 		int aInt = Integer.parseInt(aStr, 2); //int
 		int bInt = Integer.parseInt(bStr, 2); //int 
 		int result = aInt - bInt; // int
 		String resultStr = Integer.toBinaryString(result); //bin str
-		Binary resultBin = new Binary(resultStr);
-		return resultBin;
+		return new Binary(resultStr);
 	}
 
 	@Override
-	public Binary multiply(Binary a, Binary b) {
+	public Binary multiply(Object num1, Object num2) {
+		Binary a = (Binary) num1;
+		Binary b = (Binary) num2;
 		String aStr = a.getNumber(); //bin str
 		String bStr = b.getNumber(); //bin str
 		int aInt = Integer.parseInt(aStr, 2); //int
 		int bInt = Integer.parseInt(bStr, 2); //int 
 		int result = aInt * bInt; // int
 		String resultStr = Integer.toBinaryString(result); //bin str
-		Binary resultBin = new Binary(resultStr);
-		return resultBin;
+		return new Binary(resultStr);
 	}
 
 	// should i include actual decimals in divisions?
 	@Override
-	public Binary divide(Binary a, Binary b) {
+	public Binary divide(Object num1, Object num2) {
+		Binary a = (Binary) num1;
+		Binary b = (Binary) num2;
 		String aStr = a.getNumber(); //bin str
 		String bStr = b.getNumber(); //bin str
 		int aInt = Integer.parseInt(aStr, 2); //int
 		int bInt = Integer.parseInt(bStr, 2); //int 
 		int result = aInt + bInt; // int
 		String resultStr = Integer.toBinaryString(result); //bin str
-		Binary resultBin = new Binary(resultStr);
-		return resultBin;
+		return new Binary(resultStr);
 	}
 }
