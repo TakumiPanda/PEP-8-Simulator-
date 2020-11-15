8283d6e91bdec28c8650197045c8bf15f0a21eb3
danieyll <danieyll@danny.wilsons>
2020-11-14 16:44:56 -0800

    Added javadocs to Calculator extension class.

--- src/model/BinaryCalculator.java
+++ src/model/BinaryCalculator.java
@@ -1,7 +1,21 @@
 package model;
 
+/**
+ * Calculator class that'll do addition, subtraction, multiplication, 
+ * division, and modulo. Takes in two objects and returns a Binary.
+ * 
+ * @author 
+ * @version 2.3
+ *
+ */
 public class BinaryCalculator implements Calculator {
 
+	/**
+	 * Add two objects and returns a Binary.
+	 * 
+	 * @param num1 first object
+	 * @param num2 second object
+	 */
 	@Override
 	public Binary add(Object num1, Object num2) {
 		Binary a = (Binary) num1;
@@ -15,6 +29,13 @@ public class BinaryCalculator implements Calculator {
 		return new Binary(resultStr);
 	}
 
+	/**
+	 * Subtracts the second object with the first object.
+	 * Returns a Binary.
+	 * 
+	 * @param num1 first object
+	 * @param num2 second object
+	 */
 	@Override
 	public Binary subtract(Object num1, Object num2){
 		Binary a = (Binary) num1;
@@ -28,6 +49,12 @@ public class BinaryCalculator implements Calculator {
 		return new Binary(resultStr);
 	}
 
+	/**
+	 * Multiply the two objects together. Returns a Binary.
+	 * 
+	 * @param num1 first object
+	 * @param num2 second object
+	 */
 	@Override
 	public Binary multiply(Object num1, Object num2) {
 		Binary a = (Binary) num1;
@@ -41,6 +68,13 @@ public class BinaryCalculator implements Calculator {
 		return new Binary(resultStr);
 	}
 
+	/**
+	 * Divide the first object with the second object.
+	 * Returns a Binary.
+	 * 
+	 * @param num1 first object
+	 * @param num2 second object
+	 */
 	@Override
 	public Binary divide(Object num1, Object num2) {
 		Binary a = (Binary) num1;
@@ -55,6 +89,13 @@ public class BinaryCalculator implements Calculator {
 		return resultBin;
 	}
 	
+	/**
+	 * Mods the first object with the second object.
+	 * Returns a Binary.
+	 * 
+	 * @param num1 first object
+	 * @param num2 second object
+	 */
 	@Override
 	public Binary mod(Object num1, Object num2) {
 		Binary a = (Binary) num1;
