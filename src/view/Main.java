811f148cff93902da92eeb68c38f8d5ae763a782
garret123h <garret12@uw.edu>
2020-11-17 12:48:36 -1000

    Add some sample programs for on the fly testing

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,8 +1,8 @@
 package view;
 
-import java.io.IOException;
-
 import controller.Simulator;
+
+import java.io.IOException;
 
 public class Main {
 
@@ -20,14 +20,35 @@ This command will print Hello in Hex Code
 500048 500065 50006C 50006C 50006F 00
 
 Command will print Hello in Machine Code
-010100000000000001001000 010100000000000001100101 010100000000000001101100 010100000000000001101100 010100000000000001101111 000000000000000000000000
+010100000000000001001000 010100000000000001100101 010100000000000001101100 010100000000000001101100 010100000000000001101111 00000000
+
+Command will execute the Branch unconditional instruction
+000001010000000000000000 00000000
+
+Command will Load 0111 into A register than Store A register value into Memory[1010]
+110000000000000000000111 111000010000000000001010 00000000
+
+Command will Load the A register with the value of 1(in binary). Then perform an ADD operation to increment by one
+110000000000000000000001 011100000000000000000001 00000000
 
 This command will print out Hello in Assembly
-STRO 0x0048,d ; Output a 'H'
-STRO 0x0065,d ; Output a 'e'
-STRO 0x006C,d ; Output a 'l'
-STRO 0x006C,d ; Output a 'l'
-STRO 0x006F,d ; Output a 'o'
+charo 0x0048,d ; Output a 'H'
+charo 0x0065,d ; Output a 'e'
+charo 0x006C,d ; Output a 'l'
+charo 0x006C,d ; Output a 'l'
+charo 0x006F,d ; Output a 'o'
 STOP          ; End program
 .END
+
+
+Program that does something
+	CHARI   1,d
+	LDr     5,i
+while:   	LDr 4,d
+         	BREQ    endWh
+         	CHARO   1,d
+         	CHARI   1,d
+         	BR      while
+endWh:   	STOP
+
 */
