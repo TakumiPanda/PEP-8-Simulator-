5da6cd1ea2b480e9d95d93624e1fc2a82cad6a99
garret123h <garret12@uw.edu>
2020-11-20 12:23:15 -1000

    Update arithmetic operation methods to use the parent class for the actual calculation
    Why: Remove redundancy in code and reformat to look more readable.
    
    This change addresses the need by: In each operation call the parent
    classes 'executeOperation' method and pass in the operation to be
    performed.

--- src/model/BinaryCalculator.java
+++ src/model/BinaryCalculator.java
@@ -1,122 +1,36 @@
 package model;
 
-import utils.Transformer;
-
 /**
  * Calculator class that'll do addition, subtraction, multiplication, 
- * division, and modulo. Takes in two objects and returns a Binary.
+ * division, and modulo. Takes in two Binary and returns a Binary.
  * 
  * @author 
  * @version 2.3
  * 
  */
-public class BinaryCalculator implements Calculator {
+public class BinaryCalculator extends Calculator {
 
+    public BinaryCalculator() {
+        super();
+    }
 
-	/**
-	 * Add two objects and returns a Binary.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Binary add(Object num1, Object num2) {
-		String aStr = num1 + ""; //bin str
-		String bStr = num2 + ""; //bin str
-		Binary a = new Binary(aStr);
-		int aInt = Integer.parseInt(aStr, 2); //int
-		int bInt = Integer.parseInt(bStr, 2); //int
-		int result = aInt + bInt; // int
-		String resultStr = Integer.toBinaryString(result); //bin str
-		resultStr = (result < 0)? Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "1"):
-			Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "0");
-		return new Binary(resultStr);
-	}
+    public Binary add(Binary binOne, Binary binTwo) {
+        return (Binary) super.executeOperation(binOne, binTwo, 2, "Add");
+    }
 
-	/**
-	 * Subtracts the second object with the first object.
-	 * Returns a Binary.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Binary subtract(Object num1, Object num2){
-		String aStr = num1 + ""; //bin str
-		String bStr = num2 + ""; //bin str
-		Binary a = new Binary(aStr);
-		Binary b = new Binary(bStr);
-		int aInt = Integer.parseInt(aStr, 2); //int
-		int bInt = Integer.parseInt(bStr, 2); //int 
-		int result = aInt - bInt; // int
-		String resultStr = Integer.toBinaryString(result); //bin str
-		resultStr = (result < 0)? Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "1"):
-			Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "0");
-		return new Binary(resultStr);
-	}
+    public Binary subtract(Binary binOne, Binary binTwo) {
+        return (Binary) super.executeOperation(binOne, binTwo, 2, "Subtract");
+    }
 
-	/**
-	 * Multiply the two objects together. Returns a Binary.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Binary multiply(Object num1, Object num2) {
-		String aStr = num1 + ""; //bin str
-		String bStr = num2 + "";; //bin str
-		Binary a = new Binary(aStr);
-		Binary b = new Binary(bStr);
-		int aInt = Integer.parseInt(aStr, 2); //int
-		int bInt = Integer.parseInt(bStr, 2); //int 
-		int result = aInt * bInt; // int
-		String resultStr = Integer.toBinaryString(result); //bin str
-		resultStr = (result < 0)? Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "1"):
-			Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "0");
-		return new Binary(resultStr);
-	}
+    public Binary multiply(Binary binOne, Binary binTwo) {
+        return (Binary) super.executeOperation(binOne, binTwo, 2, "Multiply");
+    }
 
-	/**
-	 * Divide the first object with the second object.
-	 * Returns a Binary.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Binary divide(Object num1, Object num2) {
-		String aStr = num1 + ""; //bin str
-		String bStr = num2 + ""; //bin str
-		Binary a = new Binary(aStr);
-		Binary b = new Binary(bStr);
-		int aInt = Integer.parseInt(aStr, 2); //int
-		int bInt = Integer.parseInt(bStr, 2); //int 
-		int result = aInt / bInt; // int
-		String resultStr = Integer.toBinaryString(result); //bin str
-		resultStr = (result < 0)? Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "1"):
-			Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "0");
-		return new Binary(resultStr);
-	}
+    public Binary divide(Binary binOne, Binary binTwo) {
+        return (Binary) super.executeOperation(binOne, binTwo, 2, "Divide");
+    }
 
-	/**
-	 * Mods the first object with the second object.
-	 * Returns a Binary.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Binary mod(Object num1, Object num2) {
-		String aStr = num1 + ""; //bin str
-		String bStr = num2 + ""; //bin str
-		Binary a = new Binary(aStr);
-		Binary b = new Binary(bStr);
-		int aInt = Integer.parseInt(aStr, 2); //int
-		int bInt = Integer.parseInt(bStr, 2); //int 
-		int result = aInt % bInt; // int
-		String resultStr = Integer.toBinaryString(result); //bin str
-		resultStr = (result < 0)? Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "1"):
-			Transformer.formatBinaryAddress(resultStr, a.getNumber().length(), "0");
-		return new Binary(resultStr);
-	}
+    public Binary mod(Binary binOne, Binary binTwo) {
+        return (Binary) super.executeOperation(binOne, binTwo, 2, "Mod");
+    }
 }
--- src/model/HexCalculator.java
+++ src/model/HexCalculator.java
@@ -2,108 +2,34 @@ package model;
 
 /**
  * Calculator class that'll do addition, subtraction, multiplication, 
- * division, and modulo. Takes in two objects and returns a Hexadecimal.
+ * division, and modulo. Takes in two Hexadecimal and returns a Hexadecimal.
  * 
  * @author 
  * @version 2.3
  *
  */
-public class HexCalculator implements Calculator {
+public class HexCalculator extends Calculator {
 
-	/**
-	 * Add two objects and returns a Hexadecimal.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override 
-	public Hexadecimal add(Object num1, Object num2) {
-		Hexadecimal a = new Hexadecimal(num1 + "");
-		Hexadecimal b = new Hexadecimal(num2 + "");
-		String aStr = a.getNumber(); //Hex str
-		String bStr = b.getNumber(); //Hex str
-		int aInt = Integer.parseInt(aStr, 16); //int
-		int bInt = Integer.parseInt(bStr, 16); //int 
-		int result = aInt + bInt; // int
-		String resultStr = Integer.toHexString(result); //Hex str
-		return new Hexadecimal(resultStr);
-	}
+    public HexCalculator(){
+        super();
+    }
+    public Hexadecimal add(Hexadecimal hexOne, Hexadecimal hexTwo) {
+        return (Hexadecimal) super.executeOperation(hexOne, hexTwo, 16, "Add");
+    }
 
-	/**
-	 * Subtracts the second object with the first object.
-	 * Returns a Hexadecimal.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Hexadecimal subtract(Object num1, Object num2) {
-		Hexadecimal a = new Hexadecimal(num1 + "");
-		Hexadecimal b = new Hexadecimal(num2 + "");
-		String aStr = a.getNumber(); //Hex str
-		String bStr = b.getNumber(); //Hex str
-		int aInt = Integer.parseInt(aStr, 16); //int
-		int bInt = Integer.parseInt(bStr, 16); //int 
-		int result = aInt - bInt; // int
-		String resultStr = Integer.toHexString(result); //Hex str
-		return new Hexadecimal(resultStr);
-	}
+    public Hexadecimal subtract(Hexadecimal hexOne, Hexadecimal hexTwo) {
+        return (Hexadecimal) super.executeOperation(hexOne, hexTwo, 16, "Subtract");
+    }
 
-	/**
-	 * Multiply the two objects together. Returns a Hexadecimal.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Hexadecimal multiply(Object num1, Object num2) {
-		Hexadecimal a = new Hexadecimal(num1 + "");
-		Hexadecimal b = new Hexadecimal(num2 + "");
-		String aStr = a.getNumber(); //Hex str
-		String bStr = b.getNumber(); //Hex str
-		int aInt = Integer.parseInt(aStr, 16); //int
-		int bInt = Integer.parseInt(bStr, 16); //int 
-		int result = aInt * bInt; // int
-		String resultStr = Integer.toHexString(result); //Hex str
-		return new Hexadecimal(resultStr);
-	}
+    public Hexadecimal multiply(Hexadecimal hexOne, Hexadecimal hexTwo) {
+        return (Hexadecimal) super.executeOperation(hexOne, hexTwo, 16, "Multiply");
+    }
 
-	/**
-	 * Divide the first object with the second object.
-	 * Returns a Hexadecimal.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	public Hexadecimal divide(Object num1, Object num2) {
-		Hexadecimal a = new Hexadecimal(num1 + "");
-		Hexadecimal b = new Hexadecimal(num2 + "");
-		String aStr = a.getNumber(); //Hex str
-		String bStr = b.getNumber(); //Hex str
-		int aInt = Integer.parseInt(aStr, 16); //int
-		int bInt = Integer.parseInt(bStr, 16); //int 
-		int result = aInt / bInt; // int
-		String resultStr = Integer.toHexString(result); //Hex str
-		return new Hexadecimal(resultStr);
-	}
+    public Hexadecimal divide(Hexadecimal hexOne, Hexadecimal hexTwo) {
+        return (Hexadecimal) super.executeOperation(hexOne, hexTwo, 16, "Divide");
+    }
 
-	/**
-	 * Mods the first object with the second object.
-	 * Returns a Hexadecimal.
-	 * 
-	 * @param num1 first object
-	 * @param num2 second object
-	 */
-	@Override
-	public Hexadecimal mod(Object num1, Object num2) {
-		Hexadecimal a = new Hexadecimal(num1 + "");
-		Hexadecimal b = new Hexadecimal(num2 + "");
-		String aStr = a.getNumber(); //Hex str
-		String bStr = b.getNumber(); //Hex str
-		int aInt = Integer.parseInt(aStr, 16); //int
-		int bInt = Integer.parseInt(bStr, 16); //int 
-		int result = aInt % bInt; // int
-		String resultStr = Integer.toHexString(result); //Hex str
-		return new Hexadecimal(resultStr);
-	}
+    public Hexadecimal mod(Hexadecimal hexOne, Hexadecimal hexTwo) {
+        return (Hexadecimal) super.executeOperation(hexOne, hexTwo, 16, "Mod");
+    }
 }
